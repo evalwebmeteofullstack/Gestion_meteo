0cfd4e2b5bff1418e8aa518dce74c9ce3e88fd3a
micousin <michael.cousin81@gmail.com>
2019-02-21 15:07:14 +0100

    Formulaire inscription frontEnd terminé + Organisation des fichiers dans des dossier front et back

---
+++ Projet/FCC_meteo_FrontEnd/JS/CheckFormWithUser.js
@@ -0,0 +1,115 @@
+//Déclaration d'un objet
+var monUser;
+var boxPseudo = document.getElementById("pseudoInscr");
+var boxEmail = document.getElementById("emailInscr");
+var boxPwd1 = document.getElementById("mdp1Inscr");
+var boxPwd2 = document.getElementById("mdp2Inscr");
+var classPseudo = boxPseudo.className;
+var classEmail = boxEmail.className;
+var classPwd1 = boxPwd1.className;
+var classPwd2 = boxPwd2.className;
+
+//Fonction auto exécutante
+(function() {
+  // (utilisation des variables pour limiter la répétition du code)
+  var monForm = document.getElementById("formInscri");
+
+  // neutralisation de comportement par défaut de la soumission du formulaire
+  monForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+  });
+
+  //Ajout d'un evenement lors du submit du formulaire
+  monForm.addEventListener("submit", verifierSaisie);
+  resStyles();
+
+  //Ajout des evenements sur les textBoxs du formulaire
+  boxPseudo.addEventListener("input", verifierPseudo);
+  boxEmail.addEventListener("input", verifierEmail);
+  boxPwd1.addEventListener("input", verifierMpdOne);
+  boxPwd2.addEventListener("input", verifierMpdTwo);
+
+  //Instance d'un objet MyUser
+  monUser = new MyUser();
+})();
+
+//Fonction qui applique une classe default aux textBoxs
+function resStyles() {
+  var defaultClass = boxPseudo.className;
+  boxPseudo.className = defaultClass + " " + "unsetvalue";
+  defaultClass = boxEmail.className;
+  boxEmail.className = defaultClass + " " + "unsetvalue";
+  defaultClass = boxPwd1.className;
+  boxPwd1.className = defaultClass + " " + "unsetvalue";
+  defaultClass = boxPwd2.className;
+  boxPwd2.className = defaultClass + " " + "unsetvalue";
+}
+
+//Fonction qui appelle toutes les fonctions de vérifications des champs.
+//True => Elle envoie le formulaire.
+//False => Elle affiche une alerte.
+function verifierSaisie() {
+	var ok =
+		verifierPseudo() &
+		verifierEmail() &
+		verifierMpdOne() &
+		verifierMpdTwo();
+	if (ok) {
+		envoyerFormulaire();
+	} else {
+		alert("Erreur de saisie !!");
+	}
+}
+
+//Fonction qui retourne true ou false sur la vérification du pseudo
+//Et met en forme les textBoxs en fonction.
+function verifierPseudo() {
+  //On récupère le bool renvoyé par la fonction checkPeudo
+  //De la classe User.
+	var ok = monUser.checkPseudo(boxPseudo.value);
+	if (ok) {
+    boxPseudo.className = classPseudo + " " + "goodvalue";
+	} else {
+    boxPseudo.className = classPseudo + " " + "badvalue";
+		boxPseudo.setAttribute("placeholder",
+				"Le nom d'utilisateur doit faire au moins 1 lettre...");
+	}
+	return ok;
+}
+
+function verifierEmail() {
+	var ok = monUser.checkEmail(boxEmail.value);
+	if (ok) {
+		boxEmail.className = classEmail + " " + "goodvalue";
+	} else {
+		boxEmail.className = classEmail + " " + "badvalue";
+		boxEmail.setAttribute("placeholder", "Format de mail invalide...");
+	}
+	return ok;
+}
+
+function verifierMpdOne() {
+	var ok = monUser.checkPwd(boxPwd1.value);
+	if (ok) {
+		boxPwd1.className = classPwd1 + " " + "goodvalue";
+    verifierMpdTwo();
+	} else {
+		boxPwd1.className = classPwd1 + " " + "badvalue";
+    verifierMpdTwo();
+		boxPwd1.setAttribute("placeholder",
+				"Le mot de passe doit faire entre 8 et 256 char...");
+	}
+	return ok;
+}
+
+function verifierMpdTwo() {
+  if((boxPwd1.value === boxPwd2.value) && (monUser.checkPwd(boxPwd1.value))){
+    boxPwd2.className = classPwd2 + " " + "goodvalue";
+    return true;
+  } else {
+    boxPwd2.className = classPwd2 + " " + "badvalue";
+		boxPwd2.setAttribute("placeholder",
+				"Les mots de passe ne correspondent pas...");
+    return false;
+  }
+}
