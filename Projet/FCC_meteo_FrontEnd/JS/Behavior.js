4bace21ec0acceaa754c16a84923378b04aa4436
YannickFABRE <yannickfabre@hotmail.fr>
2019-02-25 11:22:29 +0100

    Modif suite au merge avec Yannick

--- Projet/FCC_meteo_FrontEnd/JS/Behavior.js
+++ Projet/FCC_meteo_FrontEnd/JS/Behavior.js
@@ -12,7 +12,7 @@ var pageX;
 function hideAll() {
   document.getElementById("divRech").style.display = 'none';
   document.getElementById("divLog").style.display = 'none';
-  document.getElementById("return").style.display = 'none';
+  document.getElementById("return").style.visibility = 'hidden';
   document.getElementById("divTitre").style.display = 'none';
   document.getElementById("divSem").style.display = 'none';
   document.getElementById("divFav").style.display = 'none';
@@ -60,13 +60,14 @@ function manUser1() {
 function recherche() {
   hideAll();
   document.getElementById("divRech").style.display = 'flex';
+  document.getElementById("divTitre").style.display = 'flex';
   document.getElementById("divSem").style.display = 'flex';
   page = 1;
 }
 
 function connex1() {
   hideAll();
-  document.getElementById("return").style.display = 'flex';
+  document.getElementById("return").style.visibility = 'visible';
   document.getElementById("divConnex").style.display = 'flex';
   if (page === 2 || page === 4){
     page = 3;
@@ -75,7 +76,7 @@ function connex1() {
 
 function inscr1() {
   hideAll();
-  document.getElementById("return").style.display = 'flex';
+  document.getElementById("return").style.visibility = 'visible';
   document.getElementById("divInscri").style.display = 'flex';
   if (page === 2 || page === 3){
     page = 4;
@@ -84,7 +85,7 @@ function inscr1() {
 
 function detailJour() {
   hideAll();
-  document.getElementById("return").style.display = 'flex';
+  document.getElementById("return").style.visibility = 'visible';
   document.getElementById("divTitre").style.display = 'flex';
   document.getElementById("divHeure").style.display = 'flex';
   page = 2;
